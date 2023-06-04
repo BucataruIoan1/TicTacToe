d94545e4f08807ffe5b13ef28da624f9e7cdd504
BucataruIoan1 <bucataruioan1@gmail.com>
2023-06-04 19:17:57 +0300

    Add winning checkout

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -18,8 +18,22 @@ export class BoardComponent implements OnInit {
   currentBox: number = 0;
   isBoxXPress: boolean[] = [false, false, false, false, false, false, false, false, false];
   isBoxOPress: boolean[] = [false, false, false, false, false, false, false, false, false];
+  winningCombinations: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  isBoxDisabled: boolean[] = [false, false, false, false, false, false, false, false, false];
 
   clickedBox(index: number) {
+    if(!this.isBoxDisabled[index]) {
+      this.isBoxDisabled[index] = true;
+    }
     if (this.currentTurnNumber < 9) {
       if (
         (this.currentTurnNumber % 2 === 0) &&
@@ -33,6 +47,46 @@ export class BoardComponent implements OnInit {
         this.isBoxOPress[index] = true;
       }
       this.currentTurnNumber++;
+
+      this.checkWinningCondition();
+    }
+  }
+
+  checkWinningCondition() {
+    for (let combination of this.winningCombinations) {
+      let box1 = combination[0];
+      let box2 = combination[1];
+      let box3 = combination[2];
+  
+      if (
+        this.isBoxXPress[box1] &&
+        this.isBoxXPress[box2] &&
+        this.isBoxXPress[box3]
+      ) {
+        // X player wins
+        console.log("X player wins");
+        this.resetGame();
+      } else if (
+        this.isBoxOPress[box1] &&
+        this.isBoxOPress[box2] &&
+        this.isBoxOPress[box3]
+      ) {
+        // O player wins
+        console.log("O player wins");
+        this.resetGame();
+      }
+    }
+  }
+
+  resetGame() {
+    for(let i = 0; i < this.isBoxXPress.length; i++) {
+      setTimeout(() => {
+        this.isBoxOPress[i] = false;
+        this.isBoxXPress[i] = false;
+        this.isBoxDisabled[i] = false;
+        this.currentTurnNumber = 0;
+      }, 1500)
+
     }
   }
 }
