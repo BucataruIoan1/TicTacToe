b70b90ec693cc1d8930f514b2e462abeed320564
BucataruIoan1 <bucataruioan1@gmail.com>
2023-06-04 15:10:25 +0300

    Create x and o functionality

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -14,4 +14,25 @@ export class BoardComponent implements OnInit {
 
   boxItems: any[] = Array(9).fill(0);
 
+  currentTurnNumber: number = 0;
+  currentBox: number = 0;
+  isBoxXPress: boolean[] = [false, false, false, false, false, false, false, false, false];
+  isBoxOPress: boolean[] = [false, false, false, false, false, false, false, false, false];
+
+  clickedBox(index: number) {
+    if (this.currentTurnNumber < 9) {
+      if (
+        (this.currentTurnNumber % 2 === 0) &&
+        !this.isBoxXPress[index]
+      ) {
+        this.isBoxXPress[index] = true;
+      } else if (
+        (this.currentTurnNumber % 2 !== 0) &&
+        !this.isBoxOPress[index]
+      ) {
+        this.isBoxOPress[index] = true;
+      }
+      this.currentTurnNumber++;
+    }
+  }
 }
