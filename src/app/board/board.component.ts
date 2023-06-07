b36e2bb8fb6fa6e7d0be51b9590f6623d6a83c2e
BucataruIoan1 <bucataruioan1@gmail.com>
2023-06-07 19:03:23 +0300

    Add strike functionality

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -29,6 +29,14 @@ export class BoardComponent implements OnInit {
   ];
   isBoxDisabled: boolean[] = [false, false, false, false, false, false, false, false, false];
   isGameOver: boolean = false;
+  isStrikeRow1: boolean = false;
+  isStrikeRow2: boolean = false;
+  isStrikeRow3: boolean = false;
+  isStrikeColumn1: boolean = false;
+  isStrikeColumn2: boolean = false;
+  isStrikeColumn3: boolean = false;
+  isStrikeDiagonal1: boolean = false;
+  isStrikeDiagonal2: boolean = false;
 
   clickedBox(index: number) {
     if(!this.isGameOver) {
@@ -68,6 +76,32 @@ export class BoardComponent implements OnInit {
       ) {
         // X player wins
         console.log("X player wins");
+        if(this.isBoxXPress[box1] === this.isBoxXPress[0] && this.isBoxXPress[box1] === this.isBoxXPress[1] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[2]) {
+            this.isStrikeRow1 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[3] && this.isBoxXPress[box1] === this.isBoxXPress[4] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[5]) {
+            this.isStrikeRow2 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[6] && this.isBoxXPress[box1] === this.isBoxXPress[7] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[8]) {
+            this.isStrikeRow3 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[0] && this.isBoxXPress[box1] === this.isBoxXPress[3] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[6]) {
+            this.isStrikeColumn1 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[1] && this.isBoxXPress[box1] === this.isBoxXPress[4] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[7]) {
+            this.isStrikeColumn2 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[2] && this.isBoxXPress[box1] === this.isBoxXPress[5] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[8]) {
+            this.isStrikeColumn3 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[0] && this.isBoxXPress[box1] === this.isBoxXPress[4] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[8]) {
+            this.isStrikeDiagonal1 = true;
+        } else if(this.isBoxXPress[box1] === this.isBoxXPress[2] && this.isBoxXPress[box1] === this.isBoxXPress[4] && 
+          this.isBoxXPress[box1] === this.isBoxXPress[6]) {
+            this.isStrikeDiagonal2 = true;
+        }
+        
         this.isGameOver = true;
       } else if (
         this.isBoxOPress[box1] &&
@@ -76,6 +110,31 @@ export class BoardComponent implements OnInit {
       ) {
         // O player wins
         console.log("O player wins");
+        if(this.isBoxOPress[box1] === this.isBoxOPress[0] && this.isBoxOPress[box1] === this.isBoxOPress[1] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[2]) {
+            this.isStrikeRow1 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[3] && this.isBoxOPress[box1] === this.isBoxOPress[4] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[5]) {
+            this.isStrikeRow2 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[6] && this.isBoxOPress[box1] === this.isBoxOPress[7] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[8]) {
+            this.isStrikeRow3 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[0] && this.isBoxOPress[box1] === this.isBoxOPress[3] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[6]) {
+            this.isStrikeColumn1 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[1] && this.isBoxOPress[box1] === this.isBoxOPress[4] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[7]) {
+            this.isStrikeColumn2 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[2] && this.isBoxOPress[box1] === this.isBoxOPress[5] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[8]) {
+            this.isStrikeColumn3 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[0] && this.isBoxOPress[box1] === this.isBoxOPress[4] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[8]) {
+            this.isStrikeDiagonal1 = true;
+        } else if(this.isBoxOPress[box1] === this.isBoxOPress[2] && this.isBoxOPress[box1] === this.isBoxOPress[4] && 
+          this.isBoxOPress[box1] === this.isBoxOPress[6]) {
+            this.isStrikeDiagonal2 = true;
+        }
         this.isGameOver = true;
       }
     }
@@ -88,6 +147,14 @@ export class BoardComponent implements OnInit {
         this.isBoxXPress[i] = false;
         this.isBoxDisabled[i] = false;
         this.currentTurnNumber = 0;
+        this.isStrikeRow1 = false;
+        this.isStrikeRow2 = false;
+        this.isStrikeRow3 = false;
+        this.isStrikeColumn1 = false;
+        this.isStrikeColumn2 = false;
+        this.isStrikeColumn3 = false;
+        this.isStrikeDiagonal1 = false;
+        this.isStrikeDiagonal2 = false;
         this.isGameOver = false;
       }, 500)
 
